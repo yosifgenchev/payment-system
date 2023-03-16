9dc59a31278905b576ae9afd6b9f9edb4f569e0f
yosifgenchev <yosif.genchev@gmail.com>
2023-03-16 23:49:35 +0200

    Make additional configurations to Spring application

--- src/main/java/com/paymentsystem/app/Application.java
+++ src/main/java/com/paymentsystem/app/Application.java
@@ -1,9 +1,14 @@
 package com.paymentsystem.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.paymentsystem", "com.paymentsystem.repository" })
+@EnableJpaRepositories("com.paymentsystem.repository")
+@EntityScan("com.paymentsystem.*")
 public class Application {
 
 	public static void main(String[] args) {
