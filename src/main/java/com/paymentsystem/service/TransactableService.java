373d93073c1f6864fa02ca9a50a79352dc1fe651
yosifgenchev <yosif.genchev@gmail.com>
2023-03-27 18:03:39 +0300

    Refactor TransactableService

--- src/main/java/com/paymentsystem/service/TransactableService.java
+++ src/main/java/com/paymentsystem/service/TransactableService.java
@@ -1,7 +1,5 @@
 package com.paymentsystem.service;
 
-import com.paymentsystem.dto.TransactionDTO;
-import com.paymentsystem.model.Transactable;
 import com.paymentsystem.model.Transaction;
 
 import java.util.List;
@@ -10,7 +8,7 @@ public interface TransactableService {
 
     Transaction getTransactionByReferredTransactionUUID(String referred_transaction_uuid);
 
-    Transactable convertTransactableDTOtoTransactable(TransactionDTO transactionDTO, Transaction t);
+//    Transactable convertTransactableDTOtoTransactable(TransactionDTO transactionDTO, Transaction t);
     Transaction findTransactionByUuid(String uuid);
 
     List<Transaction> findAll();
--- src/main/java/com/paymentsystem/service/impl/TransactableServiceImpl.java
+++ src/main/java/com/paymentsystem/service/impl/TransactableServiceImpl.java
@@ -1,12 +1,5 @@
 package com.paymentsystem.service.impl;
 
-import com.paymentsystem.dto.TransactionDTO;
-import com.paymentsystem.factory.AuthorizeTransactionFactory;
-import com.paymentsystem.factory.ChargeTransactionFactory;
-import com.paymentsystem.factory.RefundTransactionFactory;
-import com.paymentsystem.factory.ReversalTransactionFactory;
-import com.paymentsystem.factory.TransactableFactory;
-import com.paymentsystem.model.Transactable;
 import com.paymentsystem.model.Transaction;
 import com.paymentsystem.repository.TransactionRepository;
 import com.paymentsystem.service.TransactableService;
@@ -25,36 +18,36 @@ public class TransactableServiceImpl implements TransactableService {
         return referred_transaction_uuid != null ? findTransactionByUuid(referred_transaction_uuid) : null;
     }
 
-    @Override
-    public Transactable convertTransactableDTOtoTransactable(TransactionDTO transactionDTO, Transaction t) {
-
-        Transactable transactable = null;
-
-        TransactableFactory factory = null;
-
-        if (transactionDTO.getDtype() != null) {
-            switch (transactionDTO.getDtype()) {
-                case "AuthorizeTransaction" -> {
-                    factory = new AuthorizeTransactionFactory();
-                }
-                case "ChargeTransaction" -> {
-                    factory = new ChargeTransactionFactory();
-                }
-                case "RefundTransaction" -> {
-                    factory = new RefundTransactionFactory();
-                }
-                case "ReversalTransaction" -> {
-                    factory = new ReversalTransactionFactory();
-                }
-            }
-
-            if (factory != null) {
-                transactable = factory.createTransactable(t, transactionDTO.getCustomerEmail(), transactionDTO.getCustomerPhone(), transactionDTO.getAmount());
-            }
-
-        }
-        return transactable;
-    }
+//    @Override
+//    public Transactable convertTransactableDTOtoTransactable(TransactionDTO transactionDTO, Transaction t) {
+//
+//        Transactable transactable = null;
+//
+//        TransactableFactory factory = null;
+//
+//        if (transactionDTO.getDtype() != null) {
+//            switch (transactionDTO.getDtype()) {
+//                case "AuthorizeTransaction" -> {
+//                    factory = new AuthorizeTransactionFactory();
+//                }
+//                case "ChargeTransaction" -> {
+//                    factory = new ChargeTransactionFactory();
+//                }
+//                case "RefundTransaction" -> {
+//                    factory = new RefundTransactionFactory();
+//                }
+//                case "ReversalTransaction" -> {
+//                    factory = new ReversalTransactionFactory();
+//                }
+//            }
+//
+//            if (factory != null) {
+//                transactable = factory.createTransactable(t, transactionDTO.getCustomerEmail(), transactionDTO.getCustomerPhone(), transactionDTO.getAmount());
+//            }
+//
+//        }
+//        return transactable;
+//    }
 
     @Override
     public Transaction findTransactionByUuid(String uuid) {
