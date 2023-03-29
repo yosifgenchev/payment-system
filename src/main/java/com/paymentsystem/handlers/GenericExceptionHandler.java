f76d648d8997f5a9a937903f78c1b333f960e7f5
yosifgenchev <yosif.genchev@gmail.com>
2023-03-29 23:02:00 +0300

    Add GenericExceptionHandler to make the response more user-friendly.
    Will improve it further gradually.

---
+++ src/main/java/com/paymentsystem/handlers/GenericExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.paymentsystem.handlers;
+
+import org.springframework.data.rest.core.RepositoryConstraintViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GenericExceptionHandler extends
+        ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ RepositoryConstraintViolationException.class })
+    public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
+        RepositoryConstraintViolationException nevEx = (RepositoryConstraintViolationException) ex;
+
+        String errors = nevEx.getErrors().getAllErrors().stream()
+                .map(ObjectError::toString).collect(Collectors.joining("\n"));
+
+        return new ResponseEntity<Object>(errors, new HttpHeaders(),
+                HttpStatus.PARTIAL_CONTENT);
+    }
+}
