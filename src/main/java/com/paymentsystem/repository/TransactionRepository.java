5081730bc4ed2f86c62b783fdd20f125d6dc71c7
yosifgenchev <yosif.genchev@gmail.com>
2023-04-13 01:43:03 +0300

    Add findOlderThan method to TransactionRepository

--- src/main/java/com/paymentsystem/repository/TransactionRepository.java
+++ src/main/java/com/paymentsystem/repository/TransactionRepository.java
@@ -2,13 +2,19 @@ package com.paymentsystem.repository;
 
 import com.paymentsystem.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 
+    @Query("SELECT e FROM Transaction e WHERE e.createdDateTime < :threshold")
+    List<Transaction> findOlderThan(@Param("threshold") LocalDateTime threshold);
     Optional<Transaction> findTransactionByUuid(String uuid);
 
 }
