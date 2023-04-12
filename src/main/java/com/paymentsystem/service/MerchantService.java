5d2e805951ae313ac574305a0fbb731bc054162f
yosifgenchev <yosif.genchev@gmail.com>
2023-04-12 19:26:29 +0300

    Add updateMerchantData method to MerchantService

--- src/main/java/com/paymentsystem/service/MerchantService.java
+++ src/main/java/com/paymentsystem/service/MerchantService.java
@@ -1,12 +1,15 @@
 package com.paymentsystem.service;
 
 import com.paymentsystem.model.Merchant;
+import com.paymentsystem.model.Transaction;
 
 import java.util.Optional;
 
 public interface MerchantService {
 
     Optional<Merchant> findById(Long id);
+
+    void updateMerchantData(Long id);
 
     void save(Merchant merchant);
 }
--- src/main/java/com/paymentsystem/service/impl/MerchantServiceImpl.java
+++ src/main/java/com/paymentsystem/service/impl/MerchantServiceImpl.java
@@ -4,10 +4,13 @@ import com.paymentsystem.model.Merchant;
 import com.paymentsystem.repository.MerchantRepository;
 import com.paymentsystem.service.MerchantService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class MerchantServiceImpl implements MerchantService {
@@ -17,6 +20,27 @@ public class MerchantServiceImpl implements MerchantService {
     @Override
     public Optional<Merchant> findById(Long id) {
         return merchantRepository.findById(id);
+    }
+
+    @Override
+    public void updateMerchantData(Long id) {
+        Optional<Merchant> merchantOptional = findById(id);
+
+        if (merchantOptional.isEmpty()) {
+            return;
+        }
+
+        Merchant merchant = merchantOptional.get();
+
+        BigDecimal transactionBeforeUpdate = merchant.getTotalTransactionSum();
+
+        merchant.updateTotalTransactionSum();
+
+        BigDecimal transactionAfterUpdate = merchant.getTotalTransactionSum();
+
+        save(merchant);
+
+        log.info(String.format("Total transaction for %s changed from %s to %s", merchant.getName(), transactionBeforeUpdate, transactionAfterUpdate));
     }
 
     @Override
