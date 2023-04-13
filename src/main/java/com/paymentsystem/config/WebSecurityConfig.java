571611d3e4e4a862c3d05691ebc097436292590f
yosifgenchev <yosif.genchev@gmail.com>
2023-04-13 16:45:03 +0300

    Restrict /transactions endpoint to ADMIN role only

--- src/main/java/com/paymentsystem/config/WebSecurityConfig.java
+++ src/main/java/com/paymentsystem/config/WebSecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -13,11 +16,15 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //TODO Manage security based on roles
-        http.authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/transactions").hasRole("ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                )
                 .httpBasic()
-                .and().csrf().disable();
+                .and()
+                .csrf().disable();
         return http.build();
     }
 
